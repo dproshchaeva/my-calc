3c7d79176a8671b78af70011a72c37ce52759f57
dproshchaeva <darya.proshchaeva@yandex.ru>
2024-12-26 09:36:00 +0500

    upd

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # my-calc
+
+```commandline
+cd my-calc
+.venv/bin/pip3.9 install PySide6==6.2.0
+```
+
+```commandline
+pysyde6-rcc files.qrc > files_rc.py
+
+.venv/bin/pysyde6-rcc files.qrc -o files_rc.py
+```
---
+++ my-calc.py
@@ -0,0 +1,319 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'my-calc.ui'
+##
+## Created by: Qt User Interface Compiler version 6.0.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+import files_rc
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(300, 500)
+        MainWindow.setMinimumSize(QSize(300, 500))
+        icon = QIcon()
+        icon.addFile(u"../../../Downloads/calculate_24dp_RGB(78, 68, 72)_FILL0_wght400_GRAD0_opsz24.png", QSize(), QIcon.Normal, QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        MainWindow.setStyleSheet(u"QWidget {\n"
+"	color: rgb(78, 68, 72);\n"
+"	background-color: rgb(255, 225, 245);\n"
+"	font-family: Rubik;\n"
+"	font-size: 16pt;\n"
+"	font-weight: 600;\n"
+"}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    "
+                        "                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 "
+                        "                                                                                                                                      \n"
+"\n"
+"QPushButton {\n"
+"	background_color: transparent;\n"
+"	border: none;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: rgb(222, 191, 207)\n"
+"}\n"
+"\n"
+"QPushButton: pressed {\n"
+"    background-color: rgb(188, 145, 169)\n"
+"}")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.lbl_temp = QLabel(self.centralwidget)
+        self.lbl_temp.setObjectName(u"lbl_temp")
+        self.lbl_temp.setEnabled(True)
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.lbl_temp.sizePolicy().hasHeightForWidth())
+        self.lbl_temp.setSizePolicy(sizePolicy)
+        self.lbl_temp.setStyleSheet(u"color: rgb(187, 156, 166)")
+        self.lbl_temp.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.verticalLayout.addWidget(self.lbl_temp)
+
+        self.le_entry = QLineEdit(self.centralwidget)
+        self.le_entry.setObjectName(u"le_entry")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Maximum)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.le_entry.sizePolicy().hasHeightForWidth())
+        self.le_entry.setSizePolicy(sizePolicy1)
+        self.le_entry.setStyleSheet(u"font-size: 40pt;\n"
+"border: none;")
+        self.le_entry.setMaxLength(10)
+        self.le_entry.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.le_entry.setReadOnly(True)
+
+        self.verticalLayout.addWidget(self.le_entry)
+
+        self.layout_buttons = QGridLayout()
+        self.layout_buttons.setObjectName(u"layout_buttons")
+        self.btn_1 = QPushButton(self.centralwidget)
+        self.btn_1.setObjectName(u"btn_1")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.btn_1.sizePolicy().hasHeightForWidth())
+        self.btn_1.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_1, 3, 0, 1, 1)
+
+        self.btn_minus = QPushButton(self.centralwidget)
+        self.btn_minus.setObjectName(u"btn_minus")
+        sizePolicy2.setHeightForWidth(self.btn_minus.sizePolicy().hasHeightForWidth())
+        self.btn_minus.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_minus, 2, 3, 1, 1)
+
+        self.btn_6 = QPushButton(self.centralwidget)
+        self.btn_6.setObjectName(u"btn_6")
+        sizePolicy2.setHeightForWidth(self.btn_6.sizePolicy().hasHeightForWidth())
+        self.btn_6.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_6, 2, 2, 1, 1)
+
+        self.btn_2 = QPushButton(self.centralwidget)
+        self.btn_2.setObjectName(u"btn_2")
+        sizePolicy2.setHeightForWidth(self.btn_2.sizePolicy().hasHeightForWidth())
+        self.btn_2.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_2, 3, 1, 1, 1)
+
+        self.btn_7 = QPushButton(self.centralwidget)
+        self.btn_7.setObjectName(u"btn_7")
+        sizePolicy2.setHeightForWidth(self.btn_7.sizePolicy().hasHeightForWidth())
+        self.btn_7.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_7, 1, 0, 1, 1)
+
+        self.btn_eq = QPushButton(self.centralwidget)
+        self.btn_eq.setObjectName(u"btn_eq")
+        sizePolicy2.setHeightForWidth(self.btn_eq.sizePolicy().hasHeightForWidth())
+        self.btn_eq.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_eq, 4, 3, 1, 1)
+
+        self.btn_plusmin = QPushButton(self.centralwidget)
+        self.btn_plusmin.setObjectName(u"btn_plusmin")
+        sizePolicy2.setHeightForWidth(self.btn_plusmin.sizePolicy().hasHeightForWidth())
+        self.btn_plusmin.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_plusmin, 4, 0, 1, 1)
+
+        self.btn_c = QPushButton(self.centralwidget)
+        self.btn_c.setObjectName(u"btn_c")
+        sizePolicy2.setHeightForWidth(self.btn_c.sizePolicy().hasHeightForWidth())
+        self.btn_c.setSizePolicy(sizePolicy2)
+        self.btn_c.setCursor(QCursor(Qt.PointingHandCursor))
+
+        self.layout_buttons.addWidget(self.btn_c, 0, 0, 1, 1)
+
+        self.btn_0 = QPushButton(self.centralwidget)
+        self.btn_0.setObjectName(u"btn_0")
+        sizePolicy2.setHeightForWidth(self.btn_0.sizePolicy().hasHeightForWidth())
+        self.btn_0.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_0, 4, 1, 1, 1)
+
+        self.btn_ce = QPushButton(self.centralwidget)
+        self.btn_ce.setObjectName(u"btn_ce")
+        sizePolicy2.setHeightForWidth(self.btn_ce.sizePolicy().hasHeightForWidth())
+        self.btn_ce.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_ce, 0, 1, 1, 1)
+
+        self.btn_8 = QPushButton(self.centralwidget)
+        self.btn_8.setObjectName(u"btn_8")
+        sizePolicy2.setHeightForWidth(self.btn_8.sizePolicy().hasHeightForWidth())
+        self.btn_8.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_8, 1, 1, 1, 1)
+
+        self.btn_4 = QPushButton(self.centralwidget)
+        self.btn_4.setObjectName(u"btn_4")
+        sizePolicy2.setHeightForWidth(self.btn_4.sizePolicy().hasHeightForWidth())
+        self.btn_4.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_4, 2, 0, 1, 1)
+
+        self.btn_5 = QPushButton(self.centralwidget)
+        self.btn_5.setObjectName(u"btn_5")
+        sizePolicy2.setHeightForWidth(self.btn_5.sizePolicy().hasHeightForWidth())
+        self.btn_5.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_5, 2, 1, 1, 1)
+
+        self.btn_backspace = QPushButton(self.centralwidget)
+        self.btn_backspace.setObjectName(u"btn_backspace")
+        sizePolicy2.setHeightForWidth(self.btn_backspace.sizePolicy().hasHeightForWidth())
+        self.btn_backspace.setSizePolicy(sizePolicy2)
+        icon1 = QIcon()
+        icon1.addFile(u"../my-calc/backspace_24dp_RGB(78, 68, 72)_FILL0_wght400_GRAD0_opsz24.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.btn_backspace.setIcon(icon1)
+        self.btn_backspace.setIconSize(QSize(24, 24))
+
+        self.layout_buttons.addWidget(self.btn_backspace, 0, 2, 1, 1)
+
+        self.btn_div = QPushButton(self.centralwidget)
+        self.btn_div.setObjectName(u"btn_div")
+        sizePolicy2.setHeightForWidth(self.btn_div.sizePolicy().hasHeightForWidth())
+        self.btn_div.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_div, 0, 3, 1, 1)
+
+        self.btn_9 = QPushButton(self.centralwidget)
+        self.btn_9.setObjectName(u"btn_9")
+        sizePolicy2.setHeightForWidth(self.btn_9.sizePolicy().hasHeightForWidth())
+        self.btn_9.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_9, 1, 2, 1, 1)
+
+        self.btn_mult = QPushButton(self.centralwidget)
+        self.btn_mult.setObjectName(u"btn_mult")
+        sizePolicy2.setHeightForWidth(self.btn_mult.sizePolicy().hasHeightForWidth())
+        self.btn_mult.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_mult, 1, 3, 1, 1)
+
+        self.btn_plus = QPushButton(self.centralwidget)
+        self.btn_plus.setObjectName(u"btn_plus")
+        sizePolicy2.setHeightForWidth(self.btn_plus.sizePolicy().hasHeightForWidth())
+        self.btn_plus.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_plus, 3, 3, 1, 1)
+
+        self.btn_3 = QPushButton(self.centralwidget)
+        self.btn_3.setObjectName(u"btn_3")
+        sizePolicy2.setHeightForWidth(self.btn_3.sizePolicy().hasHeightForWidth())
+        self.btn_3.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_3, 3, 2, 1, 1)
+
+        self.btn_dot = QPushButton(self.centralwidget)
+        self.btn_dot.setObjectName(u"btn_dot")
+        sizePolicy2.setHeightForWidth(self.btn_dot.sizePolicy().hasHeightForWidth())
+        self.btn_dot.setSizePolicy(sizePolicy2)
+
+        self.layout_buttons.addWidget(self.btn_dot, 4, 2, 1, 1)
+
+
+        self.verticalLayout.addLayout(self.layout_buttons)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"my-calc", None))
+        self.lbl_temp.setText("")
+        self.le_entry.setText(QCoreApplication.translate("MainWindow", u"0", None))
+        self.btn_1.setText(QCoreApplication.translate("MainWindow", u"1", None))
+#if QT_CONFIG(shortcut)
+        self.btn_1.setShortcut(QCoreApplication.translate("MainWindow", u"1", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_minus.setText(QCoreApplication.translate("MainWindow", u"\u2212", None))
+#if QT_CONFIG(shortcut)
+        self.btn_minus.setShortcut(QCoreApplication.translate("MainWindow", u"-", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_6.setText(QCoreApplication.translate("MainWindow", u"6", None))
+#if QT_CONFIG(shortcut)
+        self.btn_6.setShortcut(QCoreApplication.translate("MainWindow", u"6", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_2.setText(QCoreApplication.translate("MainWindow", u"2", None))
+#if QT_CONFIG(shortcut)
+        self.btn_2.setShortcut(QCoreApplication.translate("MainWindow", u"2", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_7.setText(QCoreApplication.translate("MainWindow", u"7", None))
+#if QT_CONFIG(shortcut)
+        self.btn_7.setShortcut(QCoreApplication.translate("MainWindow", u"7", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_eq.setText(QCoreApplication.translate("MainWindow", u"=", None))
+#if QT_CONFIG(shortcut)
+        self.btn_eq.setShortcut(QCoreApplication.translate("MainWindow", u"=", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_plusmin.setText(QCoreApplication.translate("MainWindow", u"+/-", None))
+        self.btn_c.setText(QCoreApplication.translate("MainWindow", u"C", None))
+        self.btn_0.setText(QCoreApplication.translate("MainWindow", u"0", None))
+#if QT_CONFIG(shortcut)
+        self.btn_0.setShortcut(QCoreApplication.translate("MainWindow", u"0", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_ce.setText(QCoreApplication.translate("MainWindow", u"CE", None))
+#if QT_CONFIG(shortcut)
+        self.btn_ce.setShortcut(QCoreApplication.translate("MainWindow", u"Backspace", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_8.setText(QCoreApplication.translate("MainWindow", u"8", None))
+#if QT_CONFIG(shortcut)
+        self.btn_8.setShortcut(QCoreApplication.translate("MainWindow", u"8", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_4.setText(QCoreApplication.translate("MainWindow", u"4", None))
+#if QT_CONFIG(shortcut)
+        self.btn_4.setShortcut(QCoreApplication.translate("MainWindow", u"4", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_5.setText(QCoreApplication.translate("MainWindow", u"5", None))
+#if QT_CONFIG(shortcut)
+        self.btn_5.setShortcut(QCoreApplication.translate("MainWindow", u"5", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_backspace.setText("")
+#if QT_CONFIG(shortcut)
+        self.btn_backspace.setShortcut(QCoreApplication.translate("MainWindow", u"Left", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_div.setText(QCoreApplication.translate("MainWindow", u"/", None))
+#if QT_CONFIG(shortcut)
+        self.btn_div.setShortcut(QCoreApplication.translate("MainWindow", u"/", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_9.setText(QCoreApplication.translate("MainWindow", u"9", None))
+#if QT_CONFIG(shortcut)
+        self.btn_9.setShortcut(QCoreApplication.translate("MainWindow", u"9", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_mult.setText(QCoreApplication.translate("MainWindow", u"\u00d7", None))
+#if QT_CONFIG(shortcut)
+        self.btn_mult.setShortcut(QCoreApplication.translate("MainWindow", u"*", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_plus.setText(QCoreApplication.translate("MainWindow", u"+", None))
+#if QT_CONFIG(shortcut)
+        self.btn_plus.setShortcut(QCoreApplication.translate("MainWindow", u"+", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_3.setText(QCoreApplication.translate("MainWindow", u"3", None))
+#if QT_CONFIG(shortcut)
+        self.btn_3.setShortcut(QCoreApplication.translate("MainWindow", u"3", None))
+#endif // QT_CONFIG(shortcut)
+        self.btn_dot.setText(QCoreApplication.translate("MainWindow", u".", None))
+#if QT_CONFIG(shortcut)
+        self.btn_dot.setShortcut(QCoreApplication.translate("MainWindow", u".", None))
+#endif // QT_CONFIG(shortcut)
+    # retranslateUi
+
